7c036bd61967da4d4685499602e42a67426fc1c2
Corpachave <corpachave@gmail.com>
2026-04-01 13:30:49 +0530

    code modified

--- scripts/convert_data.py
+++ scripts/convert_data.py
@@ -1,8 +1,18 @@
 # Convert COCO to YOLO
-from ultralytics.data.converter import convert_coco
+from ultralytics.data.utils import convert_coco # type: ignore
+import os
 
-convert_coco(
-    labels_dir="raw_data/PCB_Defect/annotation",
-    save_dir="raw_data/converted_PCB_Defect"
-)
+labels_dir = "raw_data/PCB_Defect/annotation"
+save_dir = "raw_data/converted_PCB_Defect"
 
+# Check if save_dir exists and is not empty
+if os.path.exists(save_dir) and os.listdir(save_dir):
+    print(f"Skipping conversion: '{save_dir}' already contains files.")
+else:
+    os.makedirs(save_dir, exist_ok=True)
+    convert_coco(
+        labels_dir=labels_dir,
+        save_dir=save_dir
+    )
+    print("Conversion completed.")
+    print(dir(ultralytics.data)) # type: ignore
